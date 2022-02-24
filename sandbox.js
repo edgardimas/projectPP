087d0d015ba1cec9e3de9c1340fac09ca6e3d3ca
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-02-24 13:47:25 +0700

    berhasil migrate

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+//npx sequelize-cli model:generate --name Product --attributes productname:string,stock:string,price:string,description:text
+
+//npx sequelize-cli model:generate --name Transactions --attributes UserId:integer,ProductsId:integer
+
+//npx sequelize-cli model:generate --name Users --attributes name:string,password:string,email:string,Role:string
+
+//npx sequelize-cli migration:generate --name add-fk-to-product
