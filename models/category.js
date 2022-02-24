6349eb8b62d558101b99ed03e5ecca762f0bb3fd
Luthfy Chaerudy <luthfy.chaerudy218@gmail.com>
2022-02-24 16:28:39 +0700

    wakwaw

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
+  class Category extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,17 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Transaction.belongsToMany(models.User, { through: 'Transaction', foreignKey: "ProductId" });
     }
   }
-  Product.init({
-    productname: DataTypes.STRING,
-    stock: DataTypes.STRING,
-    price: DataTypes.STRING,
-    description: DataTypes.TEXT
+  Category.init({
+    name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Product',
+    modelName: 'Category',
   });
-  return Product;
+  return Category;
 };
--- sandbox.js
+++ sandbox.js
@@ -4,4 +4,12 @@
 
 //npx sequelize-cli model:generate --name Users --attributes name:string,password:string,email:string,Role:string
 
+//npx sequelize-cli model:generate --name UsersDetail --attributes profilePict:string,UsersId:integer
+
+//npx sequelize-cli model:generate --name Category --attributes name:string
+
 //npx sequelize-cli migration:generate --name add-fk-to-product
+
+//npx sequelize-cli migration:generate --name add-fk-to-product
+
+//test
